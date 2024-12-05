79caf28b8549758e5696a563623fdf91ac2ca5f2
bkcheung <b.cheung@hotmail.com>
2024-12-05 15:13:24 -0500

    feat: add welcome text on 3D model and incorporate typing animation

--- src/app/components/Scene.tsx
+++ src/app/components/Scene.tsx
@@ -3,6 +3,7 @@ import Model from "./Model"
 import { Canvas } from "@react-three/fiber"
 import { Suspense } from "react"
 import { useProgress, Html, OrbitControls } from "@react-three/drei"
+import { TypeAnimation } from 'react-type-animation';
 
 
 function Loader(){
@@ -20,11 +21,50 @@ export default function Scene(){
                <Model />
                <Html 
                 position={[-0.65,2.25, 0.51]}
-                className="text-[0.5em]"
                 transform
                 occlude
+                style={{
+                    transformStyle: 'preserve-3d',
+                    WebkitFontSmoothing: 'antialiased'
+                }}
                >
-                <div> Hi! I'm Bonnie</div>
+                <TypeAnimation
+                    sequence={["Hi!", 750, "Hi! I'm Bonnie"]}
+                    wrapper="div"
+                    speed={50}
+                    style={{
+                        fontSize: "0.5em",
+                        color: "white",
+                        textShadow: "0 0 0.1em black"
+                    }}
+                    cursor={false}
+                    preRenderFirstString={true}
+                    omitDeletionAnimation={true}
+                />
+               </Html>
+               <Html 
+                position={[-1.35,1.85, 0.51]}
+                transform
+                occlude
+                style={{
+                    transform: 'translate(50%, 50%)',
+                    width: '60px',
+                    transformStyle: 'preserve-3d',
+                    WebkitFontSmoothing: 'antialiased'
+                }}
+               >
+                <TypeAnimation
+                    sequence={["", 1700, "I love creating beautiful user experiences!"]}
+                    wrapper="div"
+                    speed={75}
+                    style={{
+                        fontSize: "0.05em",
+                        lineHeight: "1.1", 
+                        color: "white",
+                        textShadow: "0 0 0.1em black"
+                    }}
+                    cursor={false}
+                />
                </Html>
             </Suspense>
         </Canvas>
