6e31fd4c12da7da0f77c49a7f976e705ea84f972
bkcheung <b.cheung@hotmail.com>
2024-12-11 01:15:56 -0500

    feat: update what camera is looking at

--- src/app/ImacAnimation.tsx
+++ src/app/ImacAnimation.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { useThree } from '@react-three/fiber';
 import gsap from 'gsap';
 import { useEffect } from 'react';
@@ -8,10 +6,7 @@ const ImacAnimation = () => {
   const { camera } = useThree();
 
   useEffect(() => {
-    // Set initial camera position (zoomed out)
     camera.position.set(75, 75, 100);
-
-    // Create GSAP animation
     gsap.to(camera.position, {
       x: 0,
       y: 15,
@@ -19,7 +14,7 @@ const ImacAnimation = () => {
       duration: 1.5,
       ease: 'power2.inOut',
       onUpdate: () => {
-        camera.lookAt(0, 0, 0);
+        camera.lookAt(0, 10, 0);
       },
     });
   }, [camera]);
