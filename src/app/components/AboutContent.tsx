8506543dce540a50be0a5170fa7e931eb8bd8953
bkcheung <b.cheung@hotmail.com>
2024-12-13 16:32:21 -0500

    chore: run prettier

--- src/app/components/AboutContent.tsx
+++ src/app/components/AboutContent.tsx
@@ -3,13 +3,16 @@ import Skillbar from './Skillbar';
 const AboutContent = () => {
   return (
     <div className="flex w-[500rem] h-[250rem] bg-[url('/wheat-field.jpg')] bg-cover bg-center">
-      <div aria-label="about-left" className="flex flex-col w-1/2 justify-center bg-white/65 p-[20rem]">
+      <div
+        aria-label="about-left"
+        className="flex flex-col w-1/2 justify-center bg-white/65 p-[20rem]"
+      >
         <h1 className="text-[20rem]">Bonnie Cheung</h1>
         <h2 className="text-[10rem]">
           About:
           <ul className="list-disc ml-[10rem] text-[7.5rem]">
             <li>
-              Since an early age, I have loved building (& unbuilding) things; 
+              Since an early age, I have loved building (& unbuilding) things;
               sorry mom!
             </li>
             <li>
@@ -32,7 +35,10 @@ const AboutContent = () => {
         aria-label="about-right"
         className="flex flex-col w-1/2 text-[10rem] justify-end items-end p-[20rem] pb-0"
       >
-        <div aria-label="skills" className="bg-white/75 p-[10rem] rounded-[10rem] w-[65%]">
+        <div
+          aria-label="skills"
+          className="bg-white/75 p-[10rem] rounded-[10rem] w-[65%]"
+        >
           <Skillbar skill="HTML & CSS" percent={90} />
           <Skillbar skill="Javascript" percent={80} />
           <Skillbar skill="Typescript" percent={75} />
