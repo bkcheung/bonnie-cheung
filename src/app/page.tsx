3f8a4b47ea6de69459bb1a395472efefece223c3
bkcheung <b.cheung@hotmail.com>
2024-12-19 21:36:25 -0500

    feat: add page navigation instructions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Scene from './components/Scene';
+import Image from 'next/image';
 
 export default function Home() {
   const year = String(new Date().getUTCFullYear());
@@ -7,10 +8,30 @@ export default function Home() {
       <main className="h-full w-full">
         <Scene />
       </main>
-      <footer className="fixed bottom-4">
+      <footer className="fixed bottom-0 bg-white/40 w-full min-h-[3rem] flex items-center justify-between px-4">
+        <div className="flex items-center">
+          <Image 
+            src="/360.png"
+            alt="rotation"
+            width={30}
+            height={30}
+            className="p-[0.25rem] pr-[0.5rem]"
+          />
+          Click & drag
+        </div>
         <h4>
           Copyright ©<a href="https://github.com/bkcheung"> bkcheung {year}</a>
         </h4>
+        <div className="flex items-center">
+          <Image 
+            src="/zoom.png"
+            alt="zoom"
+            width={30}
+            height={30}
+            className="p-[0.25rem] pr-[0.5rem]"
+          />
+          Scroll to zoom
+        </div>
       </footer>
     </div>
   );
