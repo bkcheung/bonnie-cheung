59801dc64eec8fa68fb77d37b89367cc7890f758
bkcheung <b.cheung@hotmail.com>
2024-12-19 21:48:39 -0500

    feat: reorder items in footer, have all navigation instructions together on the right side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,39 @@
-import Scene from './components/Scene';
 import Image from 'next/image';
 
+import Scene from './components/Scene';
+
 export default function Home() {
-  const year = String(new Date().getUTCFullYear());
+  // const year = String(new Date().getUTCFullYear());
   return (
     <div className="flex h-[100vh] w-[100vw] flex-col items-center">
       <main className="h-full w-full">
         <Scene />
       </main>
       <footer className="fixed bottom-0 bg-white/40 w-full min-h-[3rem] flex items-center justify-between px-4">
-        <div className="flex items-center">
-          <Image 
-            src="/360.png"
-            alt="rotation"
-            width={30}
-            height={30}
-            className="p-[0.25rem] pr-[0.5rem]"
-          />
-          Click & drag
-        </div>
         <h4>
-          Copyright ©<a href="https://github.com/bkcheung"> bkcheung {year}</a>
+          ©<a href="https://github.com/bkcheung"> Bonnie Cheung </a>
         </h4>
-        <div className="flex items-center">
-          <Image 
-            src="/zoom.png"
-            alt="zoom"
-            width={30}
-            height={30}
-            className="p-[0.25rem] pr-[0.5rem]"
-          />
-          Scroll to zoom
+        <div className="flex">
+          <div className="flex items-center mx-4">
+            <Image
+              src="/360.png"
+              alt="rotation"
+              width={30}
+              height={30}
+              className="p-[0.25rem] pr-[0.5rem]"
+            />
+            Click & drag
+          </div>
+          <div className="flex items-center mx-4">
+            <Image
+              src="/zoom.png"
+              alt="zoom"
+              width={30}
+              height={30}
+              className="p-[0.25rem] pr-[0.5rem]"
+            />
+            Scroll to zoom
+          </div>
         </div>
       </footer>
     </div>
