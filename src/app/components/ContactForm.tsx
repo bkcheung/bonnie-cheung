61d7bff2197f027d1a9a2d5449fda77fb6fb32ad
bkcheung <b.cheung@hotmail.com>
2024-12-24 14:05:40 -0500

    feat: add ContactForm component with form to send me an email and buttons to look at github and linkedin

---
+++ src/app/components/ContactForm.tsx
@@ -0,0 +1,85 @@
+export default function ContactForm() {
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    formData.append('access_key', `${process.env.API_KEY}`);
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const response = await fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: json,
+    });
+    const result = await response.json();
+    if (result.success) {
+      console.log(result);
+    }
+  }
+
+  return (
+    <section
+        className="w-[55%] h-full p-40  bg-white/50 rounded-[10rem] text-[12rem]"
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
+    >
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-between "
+      >
+        <div className="flex flex-col">
+          <div className="flex my-20 justify-between items-center">
+            <h2>Name:</h2>
+            <input
+              type="text"
+              name="name"
+              className="w-[70%] p-20 rounded-[5rem]"
+            />
+          </div>
+          <div className="flex my-20 justify-between">
+            <h2>Email:</h2>
+            <input
+              type="email"
+              name="email"
+              className="w-[70%] p-20 rounded-[5rem]"
+            />
+          </div>
+          <div className="flex my-20 justify-between">
+            <h2>Message:</h2>
+            <textarea
+              name="message"
+              className="w-[70%] p-20 rounded-[5rem]"
+            ></textarea>
+          </div>
+        </div>
+        <div className="flex my-20 justify-between items-center">
+            <div className="flex gap-32 opacity-60">
+                <button
+                onClick={() =>
+                window
+                    .open('https://www.linkedin.com/in/cheungbonnie/', '_blank')
+                    ?.focus()
+                }
+                className="bg-[url('/linkedin.png')] bg-cover w-[20rem] h-[20rem]"
+            ></button>
+            <button
+                onClick={() =>
+                window.open('https://github.com/bkcheung', '_blank')?.focus()
+                }
+                className="bg-[url('/github.png')] bg-cover w-[20rem] h-[20rem]"
+            ></button>
+            </div>
+          
+          <button type="submit" className="bg-beige p-20 rounded-[5rem]">
+            Send Email
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+}
