0e1e5de0ab94f2b501824305fee0e7a4acdac07f
bkcheung <b.cheung@hotmail.com>
2025-01-15 12:08:34 -0500

    fix: add ktsx2loader dispose

--- src/app/components/Desk.tsx
+++ src/app/components/Desk.tsx
@@ -1,16 +1,14 @@
 import { Html, useGLTF } from '@react-three/drei';
-import { KTX2Loader } from 'three-stdlib'
 import { useThree } from '@react-three/fiber';
 import React, { useEffect, useRef, useState } from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import { Group } from 'three';
+import { KTX2Loader } from 'three-stdlib';
 
 import { frameData, homeView } from '../data';
 import moveCamera from '../moveCamera';
 import { Button } from './Buttons';
 
-const ktx2Loader = new KTX2Loader()
-ktx2Loader.setTranscoderPath('https://unpkg.com/three@0.168.0/examples/jsm/libs/basis/')
 useGLTF.preload('/imac.glb');
 
 interface DeskProps {
@@ -20,10 +18,15 @@ interface DeskProps {
 
 function Desk({ setOrbitEnabled, setActiveFrame }: DeskProps) {
   const group = useRef<Group>(null);
-  const { gl } = useThree()
-  const { scene } = useGLTF('imac.glb', true, true, (loader) => {
-    loader.setKTX2Loader(ktx2Loader.detectSupport(gl))
-  })
+  const { gl } = useThree();
+  const ktx2Loader = new KTX2Loader();
+  ktx2Loader.setTranscoderPath(
+    'https://unpkg.com/three@0.168.0/examples/jsm/libs/basis/'
+  );
+  const { scene } = useGLTF('imac.glb', undefined, undefined, (loader) => {
+    loader.setKTX2Loader(ktx2Loader.detectSupport(gl));
+    ktx2Loader.dispose();
+  });
   const [showButtons, setShowButtons] = useState(false);
   const { camera } = useThree();
 
