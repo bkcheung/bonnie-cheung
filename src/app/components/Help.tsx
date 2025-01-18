34aa421a43bf16d934cc50b5f39ea6727a5df857
bkcheung <b.cheung@hotmail.com>
2025-01-17 22:20:23 -0500

    chore: run prettier

--- src/app/components/Help.tsx
+++ src/app/components/Help.tsx
@@ -7,7 +7,7 @@ const Help = () => {
     <section>
       {showHelp && (
         <section
-        title='Click anywhere to close'
+          title="Click anywhere to close"
           className="absolute inset-0 w-full h-full bg-black/50 hover:cursor-pointer"
           onClick={() => setShowHelp(false)}
         >
@@ -15,9 +15,7 @@ const Help = () => {
             <ul className="flex flex-col p-10 gap-2 lg:gap-4 text-gray-700 list-disc text-lg lg:text-2xl">
               <li>Click and drag to rotate the view</li>
               <li>Pinch or scroll to zoom in and out</li>
-              <li>
-                Click on the iMac buttons or frames to zoom in
-              </li>
+              <li>Click on the iMac buttons or frames to zoom in</li>
               <li>Click on background to return to home</li>
             </ul>
           </div>
